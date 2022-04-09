cfdf29785e20a72bac1609b11d1edea644e4b0c8
chomosuke <a13323600@gmail.com>
2022-04-09 18:07:15 +1000

    default moved to package.json

--- backend/rest/src/index.ts
+++ backend/rest/src/index.ts
@@ -11,7 +11,7 @@ import { routeMessage } from './message';
 // command line args
 const program = new Command();
 program
-    .option('-p, --port <number>', 'the port this server will listen on', '80');
+    .option('-p, --port <number>', 'the port this server will listen on');
 program.parse();
 const options = program.opts();
 const port = z.string().parse(options['port']);
