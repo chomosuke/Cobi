024006a64609094ee2afb83c8e94e83143dd195e
chomosuke <a13323600@gmail.com>
2022-04-29 21:49:06 +1000

    configed jest coverage ignore prisma

--- backend/auth/jest.config.ts
+++ backend/auth/jest.config.ts
@@ -3,6 +3,10 @@ import { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testRegex: './tests/.*test\\.ts$',
+    coveragePathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/prisma/',
+    ],
 };
 
 // eslint-disable-next-line import/no-default-export
