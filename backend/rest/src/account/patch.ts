cf409c0140bd51950ba5958423a1c0b685fe6f5f
chomosuke <a13323600@gmail.com>
2022-07-05 23:27:10 +1200

    rest account patch conformed to new auth services api
    all rest conformed to new auth api

--- backend/rest/src/account/patch.ts
+++ backend/rest/src/account/patch.ts
@@ -3,42 +3,42 @@ import { Context } from '../context';
 import '../auth/authenticate';
 import { paths } from '../apiTypes/api.auto';
 import { PrismaClientKnownRequestError } from '../../prisma/runtime';
+import { getToken } from '../auth/getToken';
+import { changePassword } from '../auth/changePassword';
 
 type Req = paths['/account']['patch']['requestBody']['content']['application/json'];
 
 export async function patch(context: Context, req: Request, res: Response) {
     const { userId } = req;
-    const { prisma } = context;
+    const { prisma, authUrl } = context;
     // eslint-disable-next-line no-restricted-syntax
-    const { currentPassword, ...changes } = req.body as Req;
-    let user;
-    try {
-        user = await prisma.user.findUnique({
-            where: {
-                id: userId,
-            },
-            select: {
-                password: true,
-            },
-            rejectOnNotFound: true,
-        });
-    } catch (e) {
-        if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
-            res.sendStatus(409);
-            return;
-        }
-        throw e;
-    }
-    if (currentPassword !== user.password) {
+    const { currentPassword, username, password } = req.body as Req;
+    if (await getToken(authUrl, { userId, password: currentPassword }) === null) {
         res.sendStatus(401);
         return;
     }
-    await prisma.user.update({
-        where: {
-            id: userId,
-        },
-        select: { id: true },
-        data: changes,
-    });
+    if (username !== undefined) {
+        try {
+            await prisma.user.update({
+                where: { id: userId },
+                select: { id: true },
+                data: { username },
+            });
+        } catch (e) {
+            if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
+                // unique constrain violated, username taken
+                res.sendStatus(409);
+                return;
+            }
+            throw e;
+        }
+    }
+    if (password !== undefined) {
+        await changePassword(authUrl, {
+            userId,
+            currentPassword,
+            newPassword: password,
+        });
+    }
     res.sendStatus(200);
 }
--- backend/rest/tests/account/patch.test.ts
+++ backend/rest/tests/account/patch.test.ts
@@ -1,24 +1,30 @@
 import request from 'supertest';
 import { Context } from '../../src/context';
 import { constructApp } from '../../src/constructApp';
+import { changePassword } from '../../src/auth/changePassword';
 import { PrismaClient } from '../../prisma';
 import { authenticate } from '../../src/auth/authenticate';
 import { mockAuthenticate } from '../auth/mockAuthenticate';
 import { PrismaClientKnownRequestError } from '../../prisma/runtime';
+import { getToken } from '../../src/auth/getToken';
 
 jest.mock('../../src/auth/authenticate');
 const authenticateMock = authenticate as jest.MockedFunction<typeof authenticate>;
 const userId = 123;
 authenticateMock.mockImplementation(mockAuthenticate(userId));
 
+jest.mock('../../src/auth/changePassword');
+const changePasswordMock = changePassword as jest.MockedFunction<typeof changePassword>;
+
+jest.mock('../../src/auth/getToken');
+const getTokenMock = getToken as jest.MockedFunction<typeof getToken>;
+
 type Update = PrismaClient['user']['update'];
-type FindUnique = PrismaClient['user']['findUnique'];
 const mockContext = {
     authUrl: undefined,
     prisma: {
         user: {
             update: jest.fn<Promise<{ id: number }>, Parameters<Update>>(),
-            findUnique: jest.fn<Promise<{ password: string }>, Parameters<FindUnique>>(),
         },
     },
     debug: true,
@@ -26,43 +32,60 @@ const mockContext = {
 
 describe('account patch', () => {
     beforeEach(() => {
+        getTokenMock.mockReset();
         mockContext.prisma.user.update.mockReset();
-        mockContext.prisma.user.findUnique.mockReset();
+        changePasswordMock.mockReset();
         authenticateMock.mockClear();
     });
 
+    // some conditional expect for each cases
+    /* eslint-disable jest/no-conditional-expect */
     it.each([
         { username: 'username' },
         { password: 'password' },
         { username: 'username', password: 'password' },
         {},
-    ])('should make changes: %o if everything line up', async (changes) => {
+    ])('should make changes: %o if everything succeed', async (changes) => {
         const payload = {
             currentPassword: 'currentPassword',
             ...changes,
         };
         mockContext.prisma.user.update.mockResolvedValue({ id: userId });
-        mockContext.prisma.user.findUnique.mockResolvedValue({
-            password: payload.currentPassword,
-        });
+        // current password correct
+        getTokenMock.mockResolvedValue('someToken');
+        // change password success
+        changePasswordMock.mockResolvedValue(true);
         const res = await request(constructApp(mockContext as unknown as Context))
             .patch('/api/account')
             .set('Authorization', 'bearer someToken')
             .send(payload);
         expect(res.statusCode).toBe(200);
-        expect(mockContext.prisma.user.findUnique).toHaveBeenCalledWith({
-            where: { id: userId },
-            select: { password: true },
-            rejectOnNotFound: true,
-        });
-        expect(mockContext.prisma.user.update).toHaveBeenCalledTimes(1);
-        expect(mockContext.prisma.user.update).toHaveBeenCalledWith({
-            where: { id: userId },
-            data: changes,
-            select: { id: true },
-        });
         expect(authenticateMock).toHaveBeenCalled();
+        expect(getTokenMock).toHaveBeenCalledWith(
+            mockContext.authUrl,
+            { userId, password: payload.currentPassword },
+        );
+        if (changes.username !== undefined) {
+            expect(mockContext.prisma.user.update).toHaveBeenCalledTimes(1);
+            expect(mockContext.prisma.user.update).toHaveBeenCalledWith({
+                where: { id: userId },
+                data: { username: payload.username },
+                select: { id: true },
+            });
+        } else {
+            expect(mockContext.prisma.user.update).not.toHaveBeenCalled();
+        }
+        if (changes.password !== undefined) {
+            expect(changePasswordMock).toHaveBeenCalledTimes(1);
+            expect(changePasswordMock).toHaveBeenCalledWith(
+                mockContext.authUrl,
+                { userId, currentPassword: payload.currentPassword, newPassword: payload.password },
+            );
+        } else {
+            expect(changePasswordMock).not.toHaveBeenCalled();
+        }
     });
+    /* eslint-enable jest/no-conditional-expect */
 
     it('should return 401 if currentPassword is incorrect', async () => {
         const payload = {
@@ -70,15 +93,15 @@ describe('account patch', () => {
             username: 'username',
             password: 'password',
         };
-        mockContext.prisma.user.findUnique.mockResolvedValue({
-            password: 'otherPassword',
-        });
+        getTokenMock.mockResolvedValue(null);
         const res = await request(constructApp(mockContext as unknown as Context))
             .patch('/api/account')
             .set('Authorization', 'bearer someToken')
             .send(payload);
         expect(res.statusCode).toBe(401);
+        expect(getTokenMock).toHaveBeenCalled();
         expect(mockContext.prisma.user.update).not.toHaveBeenCalled();
+        expect(changePasswordMock).not.toHaveBeenCalled();
     });
 
     it('should return 409 if username conflicts', async () => {
@@ -87,7 +110,7 @@ describe('account patch', () => {
             username: 'username',
             password: 'password',
         };
-        mockContext.prisma.user.findUnique.mockRejectedValue(
+        mockContext.prisma.user.update.mockRejectedValue(
             new PrismaClientKnownRequestError('test error', 'P2002', 'random version'),
         );
         const res = await request(constructApp(mockContext as unknown as Context))
@@ -95,7 +118,9 @@ describe('account patch', () => {
             .set('Authorization', 'bearer someToken')
             .send(payload);
         expect(res.statusCode).toBe(409);
-        expect(mockContext.prisma.user.update).not.toHaveBeenCalled();
+        expect(getTokenMock).toHaveBeenCalled();
+        expect(mockContext.prisma.user.update).toHaveBeenCalled();
+        expect(changePasswordMock).not.toHaveBeenCalled();
     });
 
     it('should throw if an unknown error is thrown by prisma', async () => {
@@ -104,7 +129,7 @@ describe('account patch', () => {
             username: 'username',
             password: 'password',
         };
-        mockContext.prisma.user.findUnique.mockRejectedValue(
+        mockContext.prisma.user.update.mockRejectedValue(
             new PrismaClientKnownRequestError('test error', 'P202', 'random version'),
         );
         const res = await request(constructApp(mockContext as unknown as Context))
@@ -112,6 +137,8 @@ describe('account patch', () => {
             .set('Authorization', 'bearer someToken')
             .send(payload);
         expect(res.statusCode).toBe(500);
-        expect(mockContext.prisma.user.update).not.toHaveBeenCalled();
+        expect(getTokenMock).toHaveBeenCalled();
+        expect(mockContext.prisma.user.update).toHaveBeenCalled();
+        expect(changePasswordMock).not.toHaveBeenCalled();
     });
 });
